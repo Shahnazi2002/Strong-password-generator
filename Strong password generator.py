d9c8f7c63a489fd07165a7e4febfbf8187a07d00
Mohammad SN <shahnazi2002@gmail.com>
2021-11-08 12:50:14 +0330

    Update Strong password generator.py

--- Strong password generator.py
+++ Strong password generator.py
@@ -1,6 +1,20 @@
 from random import sample
 
 length = int(input("Please enter the desired password length: "))
-character = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%&*?"
-password = "".join(sample(character, length))
-print(password)
+lencheck = length >= 5
+if lencheck == False:
+	print("Password length must be at least 5 characters!")
+else:
+	c1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	c2 = "abcdefghijklmnopqrstuvwxyz"
+	c3 = "1234567890"
+	c4 = "!@#$%&*?"
+	c5 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%&*?"
+	step1 = "".join(sample(c1, 1))
+	step2 = "".join(sample(c2, 1))
+	step3 = "".join(sample(c3, 1))
+	step4 = "".join(sample(c4, 1))
+	step5 = "".join(sample(c4, length-4))
+	string = step1+step2+step3+step4+step5
+	password = "".join(sample(string, length))
+	print(password)
